b4bcf58e316f1d6a843682788da603cdeeced68a
vic6 <vicmatthews116@gmail.com>
2018-05-13 18:11:56 -0700

    Add login, logout and profile routes. Add users controller and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
   has_secure_password
-  has_secure_password :auth_token
+  has_secure_token :auth_token
 
   def invalidate_token
     update_columns(auth_token: nil)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get '/profile' => 'users#profile'
+  resources :users
 end
