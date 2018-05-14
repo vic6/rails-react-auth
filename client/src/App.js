e3a9ae0ccb2552e41002e9b8a6982519380fa352
vic6 <vicmatthews116@gmail.com>
2018-05-14 14:38:59 -0700

    Add component files and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
+import Auth from './modules/Auth';
 
 class App extends Component {
   render() {
