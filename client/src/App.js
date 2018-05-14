44c905f01f421bff46fb03046aea75fed03944fd
vic6 <vicmatthews116@gmail.com>
2018-05-14 15:18:24 -0700

    Add eslintrc, add linting packages to package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Redirect, Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
 import Auth from './modules/Auth';
 
 class App extends Component {
+  state = {
+    auth: Auth.isUserAuthenticated()
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>dude</h1>
       </div>
-    );
+    )
   }
 }
 
